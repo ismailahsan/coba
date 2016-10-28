d166761638d10886cc417fe4a947d6db556434c7
Ismail Ahsan Mas'ud <Ismail Ahsan Mas'ud>
2016-10-28 17:46:38 +0800

    pesannya

---
+++ MyApplication/app/src/main/java/com/example/mcliam/belajar_android_7/Headline.java
@@ -0,0 +1,16 @@
+package com.example.mcliam.belajar_android_7;
+
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Headline extends Fragment {
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.inflate(R.layout.activity_headline,container, false);
+        }
+    }
